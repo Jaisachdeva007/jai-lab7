2b328ca8e0d0f519e709a291b7434ee6f2f63d36
jaisahdeva <jaisachdeva028@gmail.com>
2025-03-24 16:53:29 -0300

    hi

--- jai-portfolio/server/api.js
+++ jai-portfolio/server/api.js
@@ -1,25 +1,14 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
-const dotenv = require("dotenv");
-const fs = require("fs");
-
-dotenv.config();
 
 const app = express();
 const router = express.Router();
 
 app.use(cors());
 
-// Example endpoint to serve JSON data from projects.json
-router.get("/projects", (req, res) => {
-  const data = fs.readFileSync("projects.json");
-  res.json(JSON.parse(data));
-});
-
-// Root test route
 router.get("/", (req, res) => {
-  res.json({ message: "Hello from Netlify Backend!" });
+  res.json({ message: "Hello from backend!" });
 });
 
 app.use("/.netlify/functions/api", router);
